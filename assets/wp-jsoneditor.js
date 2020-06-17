c252ec43758ed4127366f09d2c55723010a0bcad
crabcyborg <mike@crabcyb.org>
2020-06-17 20:50:22 -0300

    make better use of wordpress functions, re-use more code on js side as well

--- assets/wp-jsoneditor.js
+++ assets/wp-jsoneditor.js
@@ -3,36 +3,42 @@
 	const prefix = () => $('#jsoneditor-prefix').val().trim();
 	const key = () => $('#jsoneditor-key').val().trim();
 
+	/**
+	 * 
+	 * @param {string} type 
+	 * @param {object} response 
+	 * @param {function|undefined} handleSuccess 
+	 * @param {function|undefined} handleError 
+	 */
+	const jsoneditor_status = (type, response, handleSuccess, handleError) => {
+		const status = $(`#jsoneditor-${type}-status`).removeClass();
+		const success = response && response.result;
+		const color = success ? 'green' : 'red';
+
+		if(success) {
+			typeof handleSuccess === 'function' && handleSuccess();
+			status.html('success');
+		} else {
+			status.html('error'+(response && response.error ? ': '+response.error : ''));
+		}
+
+		status.css({color});
+		setTimeout(() => status.html(''), success ? 1000 : 10000);
+	};
+
 	$(document).ready(function() {
 		const target = $('#jsoneditor');
 		const editor = new JSONEditor(target[0], {});
 
 		$(document).on('click', '#jsoneditor-load', () => $.post(
 			ajaxurl, { prefix: prefix(), key: key(), action: 'jsoneditor_action_load' },
-			response => {
-				var status = $('#jsoneditor-load-status').removeClass();
-				
-				if(response && response.result) {
-					editor.set(response.payload);
-					status.html('success');
-				} else {
-					status.html('error'+(response && response.error ? ': '+response.error : ''));
-				}
-
-				status.css({color: status.html() === 'success' ? 'green' : 'red'});
-				setTimeout(() => status.html(''), 1000);
-			},
+			response => jsoneditor_status('load', response, () => editor.set(response.payload)),
 			'json'
 		));
 
 		$(document).on('click', '#jsoneditor-save', () => $.post(
-			ajaxurl, { prefix: prefix(), key: key(), json: editor.get(), action: 'jsoneditor_action_save' },
-			response => {
-				var status = $('#jsoneditor-save-status').removeClass();
-				status.html(response && response.result ? 'success' : 'error'+(response && response.error ? ': '+response.error : ''));
-				status.css({color: status.html() === 'success' ? 'green' : 'red'});
-				setTimeout(() => status.html(''), 1000);
-			},
+			ajaxurl, { prefix: prefix(), key: key(), json: editor.get() || [], action: 'jsoneditor_action_save' },
+			response => jsoneditor_status('save', response),
 			'json'
 		));
 	});
--- jsoneditor.php
+++ jsoneditor.php
@@ -23,23 +23,8 @@
 function jsoneditor_validate_prefix_and_key() {
 	$prefix = !empty($_POST['prefix']) ? sanitize_key($_POST['prefix']) : FALSE;
 	$key = !empty($_POST['key']) ? sanitize_key($_POST['key']) : FALSE;
-
-	if(!$prefix || !$key) {
-		echo json_encode(['result' => FALSE, 'error' => 'Prefix and key must both be set and only lower alphanumeric characters, dashes, and underscores are allowed.']);
-		wp_die();
-	}
-
+	if(!$prefix || !$key) wp_send_json(['result' => FALSE, 'error' => 'Prefix and key must both be set and only lower alphanumeric characters, dashes, and underscores are allowed.']);
 	return [$prefix, $key];
-}
-
-/**
- * @param string $data
- * @return bool
- */
-function jsoneditor_is_valid_json($data) {
-	if(!$data) return FALSE;
-	@json_decode($data);
-	return json_last_error() === JSON_ERROR_NONE;
 }
 
 /**
@@ -47,6 +32,8 @@ function jsoneditor_is_valid_json($data) {
  * @return array
  */
 function jsoneditor_sanitize_json($data) {
+	if(!is_array($data)) wp_send_json(['result' => FALSE, 'error' => 'The JSON data being passed is not valid.']);
+
 	$clean = [];
 	foreach($data as $key => $value) {
 		$clean_key = sanitize_key($key);
@@ -62,7 +49,7 @@ function jsoneditor_setup_menu() {
 function jsoneditor_init() {
 	define('JSONEDITOR_BASE_PATH', plugin_dir_path(__FILE__));
 	define('JSONEDITOR_BASE_URL', plugin_dir_url(__FILE__));
-	
+
 	wp_register_script('jsoneditor-js', JSONEDITOR_BASE_URL.'assets/jsoneditor/jsoneditor.min.js', null, null, true);
 	wp_register_style('jsoneditor-css', JSONEDITOR_BASE_URL.'assets/jsoneditor/jsoneditor.min.css');
 	wp_register_script('wp-jsoneditor', JSONEDITOR_BASE_URL.'assets/wp-jsoneditor.js', null, null, true);
@@ -85,29 +72,16 @@ function jsoneditor_init() {
 
 function jsoneditor_action_load() {
 	list($prefix, $key) = jsoneditor_validate_prefix_and_key();
-
 	$option = get_option("jsoneditor_{$prefix}_{$key}");
-	if($option && jsoneditor_is_valid_json($option)) {
-		echo json_encode(['result' => TRUE, 'payload' => json_decode($option)]);
-		wp_die();
-	}
-
-	echo json_encode(['result' => FALSE, 'error' => 'No JSON data found at requested key']);
-	wp_die();
+	if(!$option || !is_array($option)) wp_send_json(['result' => FALSE, 'error' => 'No JSON data found at requested key']);
+	wp_send_json(['result' => TRUE, 'payload' => $option]);
 }
 
 function jsoneditor_action_save() {	
 	list($prefix, $key) = jsoneditor_validate_prefix_and_key();
-
-	$json = !empty($_POST['json']) ? $_POST['json'] : FALSE;
-	if(!$json || !jsoneditor_is_valid_json($json)) {
-		echo json_encode(['result' => FALSE, 'error' => 'The JSON data being passed is not valid.']);
-		wp_die();
-	}
-
-	update_option("jsoneditor_{$prefix}_{$key}", json_encode(jsoneditor_sanitize_json($json)), 'no');
-	echo json_encode(['result' => TRUE]);
-	wp_die();
+	$json = jsoneditor_sanitize_json($_POST['json'] ?? FALSE);
+	update_option("jsoneditor_{$prefix}_{$key}", $json, 'no');
+	wp_send_json(['result' => TRUE]);
 }
 
 add_action('admin_menu', 'jsoneditor_setup_menu');
